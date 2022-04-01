0ad40e05502c0086fff3c73e92b008a339c860dc
halkarajuhi <100669913+halkarajuhi@users.noreply.github.com>
2022-04-01 19:06:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ if (process.env.NODE_ENV === 'production') {
  
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+  res.sendFile(path.join('build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }

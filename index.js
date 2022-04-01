f37dee65bdfad6f0cb073688f4aa80ed3a73a3ae
halkarajuhi <100669913+halkarajuhi@users.noreply.github.com>
2022-04-01 19:09:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -34,14 +34,14 @@ if (process.env.NODE_ENV === 'production') {
  
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join('build/index.html'), function(err) {
+  res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
     if (err) {
       res.status(500).send(err)
     }
   })
 })
 }
-
+consolie.log(__dirname)
 
 
 // Handling Errors

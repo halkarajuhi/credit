9535eb881fcaa11cb1049739ebce946ee9caa2af
halkarajuhi <100669913+halkarajuhi@users.noreply.github.com>
2022-04-01 20:11:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const { check } = require('express-validator');
 if (process.env.NODE_ENV === 'production') {
 
   // Serve any static files
-   app.use(express.static(path.join'build')));
+   app.use(express.static(path.join('build')));
   
 app.get('/*', (req, res) => {
   res.sendFile(path.join('build/index.html'));

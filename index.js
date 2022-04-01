47daab9148f1696aa4eecfce9b9628b83cc2f5f2
halkarajuhi <100669913+halkarajuhi@users.noreply.github.com>
2022-04-01 18:06:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,13 +31,15 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
    app.use(express.static(path.join(__dirname, 'build')));
     
-  // Handle React routing, return all requests to React app
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
-}
+ 
 
-
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build/index.html'), function(err) {
+    if (err) {
+      res.status(500).send(err)
+    }
+  })
+})
 
 
 

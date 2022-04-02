682ddf8a6463ee52b9e5249011f7e310d2c1eb7b
halkarajuhi <100669913+halkarajuhi@users.noreply.github.com>
2022-04-02 12:51:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === 'production') {
   // Serve any static files
    app.use(express.static(path.join(__dirname,'build')));
   
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, "build/index.html"));
 });
 }

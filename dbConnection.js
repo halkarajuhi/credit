04e2f58bdf72da6fae1f38fc5161ac1282699666
halkarajuhi <100669913+halkarajuhi@users.noreply.github.com>
2022-04-04 13:47:59 +0530

    Update dbConnection.js

--- dbConnection.js
+++ dbConnection.js
@@ -6,7 +6,7 @@ const db_connection = mysql
     user: "credo", // USER NAME
     database: "db_credo", // DATABASE NAME
     password: "TrxTrx@88", // DATABASE PASSWOR
-    port:"83",
+    port:"3306",
   })
   .on("error", (err) => {
     console.log("Failed to connect to Database - ", err);
